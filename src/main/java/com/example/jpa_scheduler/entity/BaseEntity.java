4f514e0f43e4ebbc0aaa24e6a19e1bbd05e707f4
peridot <peridot489@gmail.com>
2025-02-12 14:04:13 +0900

    Entities added

---
+++ src/main/java/com/example/jpa_scheduler/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.example.jpa_scheduler.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
